f946ff0773a6ddaf29421c942027ede6e6c90be8
rizwanadam <riam16cs@cmrit.ac.in>
2018-10-20 18:42:28 +0530

    Add files via upload

--- SM project.py
+++ SM project.py
@@ -5,13 +5,9 @@ import pandas as pd
 #Part-1
 #importing training set
 dataset_train = pd.read_csv("Training_set.csv")
-training_set = dataset_train.iloc[:,1:6].values
+training_set = dataset_train.iloc[:,1:2].values
 
 #print(training_set[:,4])
-for x in training_set[:,4]:
-    x=x[:-1]
-    x=float(x)
-    x=x*1000000
 
 #feature scaling using normalization
 from sklearn.preprocessing import MinMaxScaler
@@ -22,7 +18,7 @@ trainingset_scaled = sc.fit_transform(training_set)
 #60 timesteps means that the at any time t the rnn with look at the trend from 60 days back
 x_train=[]
 y_train=[]
-for i in range(60,1258):
+for i in range(61,5643):
     x_train.append(trainingset_scaled[i-60:i,0])
     y_train.append(trainingset_scaled[i,0])
 x_train,y_train = np.array(x_train),np.array(y_train)  
@@ -42,21 +38,21 @@ regressor = Sequential()
 #adding the 1st lstm layer and some dropout regularization 
 #units refers to number of neurons in the hidden layer
 #neurons are 50 to maintain high dimentionality
-regressor.add(LSTM(units = 50,return_sequences = True, input_shape=(x_train.shape[1],1)))
+regressor.add(LSTM(units = 70,return_sequences = True, input_shape=(x_train.shape[1],1)))
 regressor.add(Dropout(0.2))
 
 #adding the 2st lstm layer and some dropout regularization. 
 #No need to specify input_shape since it is automatically recognized
-regressor.add(LSTM(units = 50,return_sequences = True))
+regressor.add(LSTM(units = 70,return_sequences = True))
 regressor.add(Dropout(0.2))
 
 #adding the 3st lstm layer and some dropout regularization
-regressor.add(LSTM(units = 50,return_sequences = True))
+regressor.add(LSTM(units = 70,return_sequences = True))
 regressor.add(Dropout(0.2))
  
 #adding the 4st lstm layer and some dropout regularization 
 #return_sequences  false. no need to return any sequences for the last layer
-regressor.add(LSTM(units = 50))
+regressor.add(LSTM(units = 70))
 regressor.add(Dropout(0.2))
 
 #adding the output layer
@@ -70,7 +66,7 @@ regressor.compile(optimizer = 'adam',loss="mean_squared_error")
 regressor.fit(x_train,y_train,epochs=100, batch_size =32)
 
 #Part-3 Making the predictions and visualising the results
-dataset_test = pd.read_csv("Google_Stock_Price_Test.csv")
+dataset_test = pd.read_csv("test_set.csv")
 real_stock_price = dataset_test.iloc[:,1:2].values
 #getting the predicted stock prices
 #for vertical concatenation use axis =0
@@ -79,16 +75,16 @@ inputs = dataset_total[len(dataset_total)-len(dataset_test)-60:].values
 inputs=inputs.reshape(-1,1)
 inputs = sc.transform(inputs)
 x_test=[]
-for i in range(60,80):
+for i in range(60,78):
     x_test.append(inputs[i-60:i,0])
 x_test = np.array(x_test)
 x_test = np.reshape(x_test,(x_test.shape[0],x_test.shape[1],1))
 predicted_stock_price=regressor.predict(x_test)
 #inverse the scale to get the actual prices 
 predicted_stock_price = sc.inverse_transform(predicted_stock_price)
-plt.plot(real_stock_price,color = 'red',label ='Real Google stock price(Jan 2017)')
-plt.plot(predicted_stock_price,color = 'blue',label ='Predicted Google stock price(Jan 2017)')
-plt.title("Google stock price prediction")
+plt.plot(real_stock_price,color = 'red',label ='Real NIFTY 50 Stock Price')
+plt.plot(predicted_stock_price,color = 'blue',label ='Predicted NIFTY Stock price')
+plt.title("NIFTY 50 Stock Price prediction")
 plt.xlabel('Time')
 plt.ylabel('Stock price')
 plt.legend()
